bd1acbde329e58184984fd62ba948cdb956c4e2c
Zeglius <33781398+Zeglius@users.noreply.github.com>
2024-07-20 19:39:52 +0200

    Squash merge dev into main

--- build.sh
+++ build.sh
@@ -4,20 +4,13 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+(
+    # https://github.com/thesofproject/sof-bin
+    cd "$(mktemp -d)"
+    wget -o sof-bin.tar.gz \
+        https://github.com/thesofproject/sof-bin/releases/download/v2024.06/sof-bin-2024.06.tar.gz
+    mkdir sof-bin
+    tar -C sof-bin -zvf sof-bin.tar.gz --strip-components=1
+    cd ./sof-bin
+    ./install.sh
+)
