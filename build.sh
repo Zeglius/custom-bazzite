b2855bf3090c3f676d22a9040009f1fbe2010bdb
Zeglius <33781398+Zeglius@users.noreply.github.com>
2024-07-22 15:13:13 +0200

    ci: Update download source link in build script

--- build.sh
+++ build.sh
@@ -6,12 +6,14 @@ RELEASE="$(rpm -E %fedora)"
 
 (
     # https://github.com/thesofproject/sof-bin
+    SOURCE=$(
+        curl -L https://api.github.com/repos/thesofproject/sof-bin/releases/latest |
+            jq -r '.assets[] | select(.name| test(".*.tar.gz$")).browser_download_url'
+    )
     cd "$(mktemp -d)"
-    wget -o sof-bin.tar.gz \
-        -A "*.tar.gz" \
-        https://github.com/thesofproject/sof-bin/releases/download/v2024.06/sof-bin-2024.06.tar.gz
-    mkdir sof-bin
-    tar -C sof-bin -xavf sof-bin.tar.gz --strip-components=1
-    cd ./sof-bin
+    wget -o sof_bin.tar.gz -A "*.tar.gz" "$SOURCE"
+    # "https://github.com/thesofproject/sof-bin/releases/download/v2024.06/sof-bin-2024.06.tar.gz"
+    mkdir sof_bin && tar -C sof_bin -xavf sof_bin.tar.gz --strip-components=1
+    cd ./sof_bin
     ./install.sh
 )
