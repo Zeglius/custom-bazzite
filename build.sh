0d5923428085b328b90a0ec738d5275733e566a4
Zeglius <33781398+Zeglius@users.noreply.github.com>
2024-07-22 15:18:49 +0200

    ci: Update download method in build script

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ RELEASE="$(rpm -E %fedora)"
             jq -r '.assets[] | select(.name| test(".*.tar.gz$")).browser_download_url'
     )
     cd "$(mktemp -d)"
-    wget -o sof_bin.tar.gz -A "*.tar.gz" "$SOURCE"
+    curl -Lo sof_bin.tar.gz -A "*.tar.gz" "$SOURCE"
     # "https://github.com/thesofproject/sof-bin/releases/download/v2024.06/sof-bin-2024.06.tar.gz"
     mkdir sof_bin && tar -C sof_bin -xavf sof_bin.tar.gz --strip-components=1
     cd ./sof_bin
